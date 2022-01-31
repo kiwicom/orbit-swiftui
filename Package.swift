3e247da9cee25f5188fe3db62917eb0c3ebe96ba
Pavel Holec <pavel.holec@kiwi.com>
2022-01-31 09:27:41 +0100

    Remove Lottie and animations.

--- Package.swift
+++ Package.swift
@@ -9,19 +9,14 @@ let package = Package(
         .library(name: "OrbitDynamic", type: .dynamic, targets: ["Orbit"]),
         .library(name: "OrbitStatic", type: .static, targets: ["Orbit"]),
     ],
-    dependencies: [
-        .package(name: "Lottie", url: "https://github.com/airbnb/lottie-ios.git", from: "3.2.3"),
-    ],
     targets: [
         .target(
             name: "Orbit",
-            dependencies: ["Lottie"],
             resources: [
                 .copy("Foundation/Typography/CircularPro-Bold.otf"),
                 .copy("Foundation/Typography/CircularPro-Medium.otf"),
                 .copy("Foundation/Typography/CircularPro-Book.otf"),
                 .copy("Foundation/Icons/Icons.ttf"),
-                .copy("Support/Animations/Airport.json"),
             ]
         ),
     ]
