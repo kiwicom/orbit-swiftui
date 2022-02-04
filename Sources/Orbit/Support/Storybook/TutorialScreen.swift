5610b5454ea1e0cceafe291be235f9e835790401
Pavel Holec <pavel.holec@kiwi.com>
2022-02-04 09:23:36 +0100

    Add simple tutorial screen for readme.

---
+++ Sources/Orbit/Support/Storybook/TutorialScreen.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+
+struct TutorialScreen: View {
+
+    var primaryAction = {}
+    var alertAction = {}
+    var secondaryAction = {}
+    
+    var body: some View {
+        VStack(spacing: .large) {
+            Card("Card Title", action: .buttonLink("Edit", action: {})) {
+                List {
+                    ListItem("List Item 1", icon: .check, style: .custom(textColor: .greenNormal))
+                    ListItem("List Item 2 with a <a href=\"...\">TextLink</a>", icon: .check)
+                    ListItem("List Item 3")
+                }
+                
+                Alert(
+                    "Alert",
+                    description: "Alert Desctiption with a <a href=\"...\">TextLink</a>",
+                    buttons: .primary("Action", action: alertAction)
+                )
+                
+                HStack(spacing: .medium) {
+                    Button("Secondary Button", style: .secondary, action: secondaryAction)
+                    Button(.creditCard, style: .status(.warning))
+                }
+            }
+            
+            VStack(alignment: .leading, spacing: .medium) {
+                Text("""
+                    Multi-line very long text containing <a href="...">TextLink</a> \
+                    and <a href="...">Some other TextLink</a>"
+                    """
+                )
+                
+                Badge("Badge", style: .status(.info, inverted: true))
+                
+                Button("Primary Button", action: primaryAction)
+            }
+            .padding(.horizontal, .medium)
+        }
+        .padding(.vertical, .medium)
+        .background(Color.cloudLight)
+    }
+}
+
+struct TutorialScreenPreviews: PreviewProvider {
+    static var previews: some View {
+        PreviewWrapper {
+            TutorialScreen()
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
