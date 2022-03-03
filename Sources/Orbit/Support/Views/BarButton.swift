a8df2d9cb380e3e5c9d3f2ac83bfa4be42ff9e49
Šimon Javora <simon.javora@kiwi.com>
2022-03-03 11:56:56 +0100

    Added `BarButton` as a way to add icon buttons to the navigation bar.

---
+++ Sources/Orbit/Support/Views/BarButton.swift
@@ -0,0 +1,57 @@
+import SwiftUI
+
+public struct BarButton: View {
+
+    private let symbol: Icon.Symbol
+    private let action: () -> Void
+
+    public var body: some View {
+        SwiftUI.Button(
+            action: {
+                HapticsProvider.sendHapticFeedback(.light(0.5))
+                action()
+            },
+            label: {
+                Icon.Content.icon(symbol, size: .medium, color: .inkNormal).view()
+            }
+        )
+        .buttonStyle(NavigateButton.OrbitStyle())
+    }
+
+    public init(_ symbol: Icon.Symbol, action: @escaping () -> Void = {}) {
+        self.symbol = symbol
+        self.action = action
+    }
+}
+
+// MARK: - Types
+private extension NavigateButton {
+
+    struct OrbitStyle: ButtonStyle {
+
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .foregroundColor(configuration.isPressed ? .inkLighter : .inkNormal)
+        }
+    }
+}
+
+// MARK: - Previews
+struct BarButtonPreviews: PreviewProvider {
+
+    public static var previews: some View {
+        PreviewWrapper {
+            BarButton(.download)
+            BarButton(.exchange)
+            BarButton(.attachment)
+            
+            NavigationView {
+                EmptyView()
+                    .navigationBarTitle("Title", displayMode: .inline)
+                    .navigationBarItems(leading: BarButton(.markdown))
+                    .navigationBarItems(trailing: BarButton(.passengers))
+            }
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
