e07279fc5ce3095f2b4199e412de32e17430e4c7
Pavel Holec <pavel.holec@kiwi.com>
2022-05-12 18:05:25 +0200

    Added NotificationBadge component

--- Sources/Orbit/Support/Storybook/Storybook+Items.swift
+++ Sources/Orbit/Support/Storybook/Storybook+Items.swift
@@ -46,6 +46,7 @@ extension Storybook {
         case listChoice
         case loading
         case navigationBar
+        case notificationBadge
         case pictureCard
         case radio
         case ratingStars
@@ -71,46 +72,47 @@ extension Storybook {
 
         var tabs: [String] {
             switch self {
-                case .colors:           return ["Basic", "Status", "Gradient"]
-                case .icons:            return ["Basic"]
-                case .illustrations:    return ["Basic"]
-                case .typography:       return ["Text", "Heading"]
+                case .colors:               return ["Basic", "Status", "Gradient"]
+                case .icons:                return ["Basic"]
+                case .illustrations:        return ["Basic"]
+                case .typography:           return ["Text", "Heading"]
 
-                case .alert:            return ["Basic", "Inline", "Mix", "Live"]
-                case .badge:            return ["Basic", "Gradient", "Mix"]
-                case .badgeList:        return ["Basic"]
-                case .button:           return ["Basic", "Status", "Gradient"]
-                case .buttonLink:       return ["Basic", "Status", "Sizes"]
-                case .card:             return ["Basic"]
-                case .carrierLogo:      return ["Basic"]
-                case .checkbox:         return ["Basic"]
-                case .choiceTile:       return ["Basic", "Centered", "Mix"]
-                case .countryFlag:      return ["Basic"]
-                case .dialog:           return ["Basic"]
-                case .emptyState:       return ["Basic"]
-                case .heading:          return ["Basic"]
-                case .horizontalScroll: return ["Basic"]
-                case .icon:             return ["Basic", "Mix"]
-                case .illustration:     return ["Basic"]
-                case .inputField:       return ["Basic", "Inline", "Password", "Mix"]
-                case .keyValue:         return ["Basic"]
-                case .list:             return ["Basic", "Mix"]
-                case .listChoice:       return ["Basic", "Button", "Checkbox", "Mix"]
-                case .radio:            return ["Basic"]
-                case .select:           return ["Basic", "Mix"]
-                case .separator:        return ["Basic", "Mix"]
-                case .skeleton:         return ["Basic", "Atomic"]
-                case .socialButton:     return ["Basic"]
-                case .`switch`:         return ["Basic"]
-                case .tabs:             return ["Basic", "Live"]
-                case .tag:              return ["Basic"]
-                case .text:             return ["Basic"]
-                case .textLink:         return ["Basic", "Live"]
-                case .tile:             return ["Basic", "Mix"]
-                case .tileGroup:        return ["Basic"]
-                case .timeline:         return ["Basic", "Mix"]
-                case .toast:            return ["Basic", "Live"]
-                default:                return []
+                case .alert:                return ["Basic", "Inline", "Mix", "Live"]
+                case .badge:                return ["Basic", "Gradient", "Mix"]
+                case .badgeList:            return ["Basic"]
+                case .button:               return ["Basic", "Status", "Gradient"]
+                case .buttonLink:           return ["Basic", "Status", "Sizes"]
+                case .card:                 return ["Basic"]
+                case .carrierLogo:          return ["Basic"]
+                case .checkbox:             return ["Basic"]
+                case .choiceTile:           return ["Basic", "Centered", "Mix"]
+                case .countryFlag:          return ["Basic"]
+                case .dialog:               return ["Basic"]
+                case .emptyState:           return ["Basic"]
+                case .heading:              return ["Basic"]
+                case .horizontalScroll:     return ["Basic"]
+                case .icon:                 return ["Basic", "Mix"]
+                case .illustration:         return ["Basic"]
+                case .inputField:           return ["Basic", "Inline", "Password", "Mix"]
+                case .keyValue:             return ["Basic"]
+                case .list:                 return ["Basic", "Mix"]
+                case .listChoice:           return ["Basic", "Button", "Checkbox", "Mix"]
+                case .notificationBadge:    return ["Basic", "Gradient", "Mix"]
+                case .radio:                return ["Basic"]
+                case .select:               return ["Basic", "Mix"]
+                case .separator:            return ["Basic", "Mix"]
+                case .skeleton:             return ["Basic", "Atomic"]
+                case .socialButton:         return ["Basic"]
+                case .`switch`:             return ["Basic"]
+                case .tabs:                 return ["Basic", "Live"]
+                case .tag:                  return ["Basic"]
+                case .text:                 return ["Basic"]
+                case .textLink:             return ["Basic", "Live"]
+                case .tile:                 return ["Basic", "Mix"]
+                case .tileGroup:            return ["Basic"]
+                case .timeline:             return ["Basic", "Mix"]
+                case .toast:                return ["Basic", "Live"]
+                default:                    return []
             }
         }
 
@@ -160,6 +162,9 @@ extension Storybook {
                 case (.listChoice, 1):          ListChoicePreviews.storybookButton
                 case (.listChoice, 2):          ListChoicePreviews.storybookCheckbox
                 case (.listChoice, 3):          ListChoicePreviews.storybookMix
+                case (.notificationBadge, 0):   NotificationBadgePreviews.storybook
+                case (.notificationBadge, 1):   NotificationBadgePreviews.storybookGradient
+                case (.notificationBadge, 2):   NotificationBadgePreviews.storybookMix
                 case (.radio, _):               RadioPreviews.storybook
                 case (.select, 0):              SelectPreviews.storybook
                 case (.select, 1):              SelectPreviews.storybookMix
@@ -188,46 +193,47 @@ extension Storybook {
 
         var sfSymbol: String {
             switch self {
-                case .colors:           return "paintpalette.fill"
-                case .icons:            return "info.circle.fill"
-                case .illustrations:    return "photo.on.rectangle.angled"
-                case .typography:       return "textformat.size"
+                case .colors:               return "paintpalette.fill"
+                case .icons:                return "info.circle.fill"
+                case .illustrations:        return "photo.on.rectangle.angled"
+                case .typography:           return "textformat.size"
 
-                case .alert:            return "exclamationmark.triangle.fill"
-                case .badge:            return "capsule.inset.filled"
-                case .badgeList:        return "checklist"
-                case .button:           return "rectangle.and.hand.point.up.left.fill"
-                case .buttonLink:       return "character.textbox"
-                case .card:             return "list.dash.header.rectangle"
-                case .carrierLogo:      return "airplane.circle.fill"
-                case .checkbox:         return "checkmark.square"
-                case .choiceTile:       return "rectangle.badge.checkmark"
-                case .countryFlag:      return "flag.fill"
-                case .dialog:           return "text.bubble.fill"
-                case .emptyState:       return "icloud.slash"
-                case .heading:          return "textformat.size.larger"
-                case .horizontalScroll: return "rectangle.lefthalf.inset.filled.arrow.left"
-                case .icon:             return "info.circle.fill"
-                case .illustration:     return "photo.on.rectangle.angled"
-                case .inputField:       return "rectangle.and.pencil.and.ellipsis"
-                case .keyValue:         return "textformat.123"
-                case .list:             return "list.bullet"
-                case .listChoice:       return "rectangle.grid.1x2"
-                case .radio:            return "smallcircle.filled.circle.fill"
-                case .select:           return "rectangle.arrowtriangle.2.outward"
-                case .separator:        return "directcurrent"
-                case .skeleton:         return "rectangle.dashed"
-                case .socialButton:     return "applelogo"
-                case .`switch`:         return "switch.2"
-                case .tabs:             return "menubar.rectangle"
-                case .tag:              return "123.rectangle.fill"
-                case .text:             return "text.alignleft"
-                case .textLink:         return "text.redaction"
-                case .tile:             return "rectangle"
-                case .tileGroup:        return "rectangle.grid.2x2"
-                case .timeline:         return "calendar.day.timeline.left"
-                case .toast:            return "ellipsis.rectangle.fill"
-                default:                return ""
+                case .alert:                return "exclamationmark.triangle.fill"
+                case .badge:                return "capsule.inset.filled"
+                case .badgeList:            return "checklist"
+                case .button:               return "rectangle.and.hand.point.up.left.fill"
+                case .buttonLink:           return "character.textbox"
+                case .card:                 return "list.dash.header.rectangle"
+                case .carrierLogo:          return "airplane.circle.fill"
+                case .checkbox:             return "checkmark.square"
+                case .choiceTile:           return "rectangle.badge.checkmark"
+                case .countryFlag:          return "flag.fill"
+                case .dialog:               return "text.bubble.fill"
+                case .emptyState:           return "icloud.slash"
+                case .heading:              return "textformat.size.larger"
+                case .horizontalScroll:     return "rectangle.lefthalf.inset.filled.arrow.left"
+                case .icon:                 return "info.circle.fill"
+                case .illustration:         return "photo.on.rectangle.angled"
+                case .inputField:           return "rectangle.and.pencil.and.ellipsis"
+                case .keyValue:             return "textformat.123"
+                case .list:                 return "list.bullet"
+                case .listChoice:           return "rectangle.grid.1x2"
+                case .notificationBadge:    return "3.circle.fill"
+                case .radio:                return "smallcircle.filled.circle.fill"
+                case .select:               return "rectangle.arrowtriangle.2.outward"
+                case .separator:            return "directcurrent"
+                case .skeleton:             return "rectangle.dashed"
+                case .socialButton:         return "applelogo"
+                case .`switch`:             return "switch.2"
+                case .tabs:                 return "menubar.rectangle"
+                case .tag:                  return "123.rectangle.fill"
+                case .text:                 return "text.alignleft"
+                case .textLink:             return "text.redaction"
+                case .tile:                 return "rectangle"
+                case .tileGroup:            return "rectangle.grid.2x2"
+                case .timeline:             return "calendar.day.timeline.left"
+                case .toast:                return "ellipsis.rectangle.fill"
+                default:                    return ""
             }
         }
 
