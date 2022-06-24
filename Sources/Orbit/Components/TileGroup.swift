b888701114bde633c1febf35b726235e67fcf3d5
Šimon Javora <simon.javora@kiwi.com>
2022-06-24 12:47:27 +0200

    Replaced `Card` in TileGroup with elevation.

--- Sources/Orbit/Components/TileGroup.swift
+++ Sources/Orbit/Components/TileGroup.swift
@@ -15,10 +15,19 @@ public struct TileGroup<Content: View>: View {
 
     public var body: some View {
         if isEmpty == false {
-            Card(borderStyle: .default, status: status, width: width, backgroundColor: backgroundColor, contentLayout: .fill) {
+            VStack(alignment: .leading, spacing: 0) {
                 content
+                    .environment(\.isInsideTileGroup, true)
             }
+            .clipShape(clipShape)
+            .elevation(.level1)
         }
+    }
+
+    var clipShape: some InsettableShape {
+        RoundedRectangle(cornerRadius: BorderRadius.default)
+            // hide the last separator automatically
+            .inset(by: Separator.Thickness.default.value)
     }
 
     var isEmpty: Bool {
