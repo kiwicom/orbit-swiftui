fcf0e4870fa94c35f1fc9607f029473b5adc71bb
Pavel Holec <pavel.holec@kiwi.com>
2022-11-30 13:17:17 +0100

    Update BarButton

--- Sources/Orbit/Support/Components/BarButton.swift
+++ Sources/Orbit/Support/Components/BarButton.swift
@@ -3,36 +3,66 @@ import SwiftUI
 /// Button for NavigationBar.
 public struct BarButton: View {
 
-    private let symbol: Icon.Symbol
+    private let content: Icon.Content
     private let action: () -> Void
 
     public var body: some View {
-        SwiftUI.Button(
-            action: {
-                HapticsProvider.sendHapticFeedback(.light(0.5))
-                action()
-            },
-            label: {
-                Icon(symbol, size: .large)
-            }
-        )
-        .buttonStyle(NavigateButton.OrbitStyle())
+        SwiftUI.Button {
+            HapticsProvider.sendHapticFeedback(.light(0.5))
+            action()
+        } label: {
+            Icon(content: content)
+        }
+        .buttonStyle(.barButton(color: content.color))
     }
 
-    public init(_ symbol: Icon.Symbol, action: @escaping () -> Void = {}) {
-        self.symbol = symbol
+    public init(_ content: Icon.Content, action: @escaping () -> Void = {}) {
+        self.content = content
         self.action = action
     }
 }
 
 // MARK: - Types
-private extension NavigateButton {
+private extension BarButton {
 
-    struct OrbitStyle: ButtonStyle {
+    struct ButtonStyle: SwiftUI.ButtonStyle {
+
+        var color: Color?
 
         func makeBody(configuration: Configuration) -> some View {
             configuration.label
-                .foregroundColor(configuration.isPressed ? .inkLight : .inkDark)
+                .foregroundColor(foregroundColor(configuration: configuration))
+        }
+
+        func foregroundColor(configuration: Configuration) -> Color? {
+            let foregroundColor: Color
+
+            if let color = color {
+                foregroundColor = color
+            } else {
+                foregroundColor = .inkDark
+            }
+
+            return configuration.isPressed ? foregroundColor.opacity(0.6) : foregroundColor
+        }
+    }
+}
+
+extension SwiftUI.ButtonStyle where Self == BarButton.ButtonStyle {
+
+    static func barButton(color: Color?) -> Self {
+        Self(color: color)
+    }
+}
+
+// MARK: - Types
+public extension Icon.Content {
+
+    var color: Color? {
+        switch self {
+            case .symbol(_, let color):             return color
+            case .image, .countryFlag:              return nil
+            case .sfSymbol(_, let color):           return color
         }
     }
 }
@@ -45,12 +75,24 @@ struct BarButtonPreviews: PreviewProvider {
             BarButton(.download)
             BarButton(.exchange)
             BarButton(.attachment)
-            
+
             NavigationView {
                 Color.cloudNormal
                     .navigationBarTitle("Title", displayMode: .inline)
-                    .navigationBarItems(leading: BarButton(.markdown))
-                    .navigationBarItems(trailing: BarButton(.passengers))
+                    .navigationBarItems(
+                        leading: HStack(spacing: .small) {
+                            BarButton(.grid)
+                            BarButton(.markdown)
+                            BarButton(.grid)
+                        }
+                    )
+                    .navigationBarItems(
+                        trailing: HStack(spacing: .small) {
+                            BarButton(.grid)
+                            BarButton(.passengers)
+                            BarButton(.grid)
+                        }
+                    )
             }
         }
         .previewLayout(.sizeThatFits)
