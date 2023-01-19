59e84027ef802b687eb6dac1eadfe1ec987f26f0
Pavel Holec <pavel.holec@kiwi.com>
2023-01-19 09:28:26 +0100

    Fix padding for ListChoice custom header content

--- Sources/Orbit/Components/ListChoice.swift
+++ Sources/Orbit/Components/ListChoice.swift
@@ -86,13 +86,13 @@ public struct ListChoice<HeaderContent: View, Content: View>: View {
         if isHeaderEmpty == false || isCustomHeaderEmpty == false {
             HStack(spacing: 0) {
                 headerTexts
+                    .padding(.trailing, .xSmall)
 
                 if isHeaderEmpty == false, idealSize.horizontal == nil {
                     Spacer(minLength: 0)
                 }
 
                 headerContent
-                    .padding(.leading, .xSmall)
                     .padding(.leading, isHeaderEmpty ? .medium : 0)
                     .padding(.trailing, disclosure == .none ? .medium : 0)
                     .accessibility(.listChoiceValue)
