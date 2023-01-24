16b70c9cbd2187076c6ea43cbf78472cfe6195c7
Šimon Javora <s.javora@me.com>
2023-01-24 09:06:26 +0100

    Update CI simulator versions to 16.2.

--- Automation/regenerate_snapshots.sh
+++ Automation/regenerate_snapshots.sh
@@ -6,8 +6,8 @@ SCHEME="Orbit-Package"
 SNAPSHOTS_DIR="Snapshots"
 
 CONFIGURATIONS=(
-    "iPhone SE (3rd generation)" "16.0"
-    "iPad (9th generation)" "16.0"
+    "iPhone SE (3rd generation)" "16.2"
+    "iPad (9th generation)" "16.2"
 )
 
 echo "Verifying all runtimes are available..."
@@ -21,17 +21,17 @@ done
 for (( i=0; i<${#CONFIGURATIONS[@]}; i+=2 )); do
     SIMULATOR_NAME=${CONFIGURATIONS[i]}
     IOS_VERSION=${CONFIGURATIONS[i+1]}
-    
+
     echo "Setting up simulator of $SIMULATOR_NAME ($IOS_VERSION) ..."
-    
+
     SIMCTL_RUNTIME_VERSION=$(echo "$IOS_VERSION" | tr "." "-")
     RUNTIME="com.apple.CoreSimulator.SimRuntime.iOS-$SIMCTL_RUNTIME_VERSION"
     SIMULATOR_ID=$(xcrun simctl create "$SIMULATOR_NAME" "$SIMULATOR_NAME" "$RUNTIME")
-    
+
     echo "Generating snapshots for $SIMULATOR_NAME ($IOS_VERSION) ..."
-    
+
     xcodebuild test -quiet -scheme $SCHEME -destination "platform=iOS Simulator,id=$SIMULATOR_ID,OS=$IOS_VERSION" OTHER_SWIFT_FLAGS="-D XCODE_IS_SNAPSHOTS_RECORDING" | grep -vE 'sec,|started|Test session results|.xcresult'
-    
+
     git -C $SNAPSHOTS_DIR/ add .
     git commit -m "Snapshots - $SIMULATOR_NAME" -q || true
     git push
