6ba13c3b36d9914a024466a4c5643d401e12078c
Pavel Holec <pavel.holec@kiwi.com>
2023-03-23 07:33:59 +0100

    Update Dialog component with link action. Remove alignment.

--- Sources/Orbit/Foundation/Colors/Tokens.swift
+++ Sources/Orbit/Foundation/Colors/Tokens.swift
@@ -4,4 +4,7 @@ public extension Color {
 
     /// Orbit screen background color (darker in both `light` and `dark` modes).
     static let screen = Color("Screen", bundle: .orbit)
+
+    /// Orbit screen modal overlay color.
+    static let overlay = Color.inkDark.opacity(0.45)
 }
