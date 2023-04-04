8c0f013c56ff6cfadda436d8f3a8c15d9ca5e358
Pavel Holec <pavel.holec@kiwi.com>
2023-04-05 00:44:47 +0200

    Add uiColor to generated colors

--- Sources/Orbit/Foundation/Colors/Colors.swift
+++ Sources/Orbit/Foundation/Colors/Colors.swift
@@ -178,4 +178,89 @@ public extension Color {
     static var whiteHover = Color("White Hover", bundle: .orbit)
     /// Orbit White Lighter color.
     static var whiteLighter = Color("White Lighter", bundle: .orbit)
+
+    /// Matching UI color.
+    var uiColor: UIColor {
+        switch self {
+            case .blueDark: return.blueDark
+            case .blueDarkActive: return.blueDarkActive
+            case .blueDarkHover: return.blueDarkHover
+            case .blueDarker: return.blueDarker
+            case .blueLight: return.blueLight
+            case .blueLightActive: return.blueLightActive
+            case .blueLightHover: return.blueLightHover
+            case .blueNormal: return.blueNormal
+            case .blueNormalActive: return.blueNormalActive
+            case .blueNormalHover: return.blueNormalHover
+            case .bundleBasic: return.bundleBasic
+            case .bundleMedium: return.bundleMedium
+            case .cloudDark: return.cloudDark
+            case .cloudDarkActive: return.cloudDarkActive
+            case .cloudDarkHover: return.cloudDarkHover
+            case .cloudLight: return.cloudLight
+            case .cloudLightActive: return.cloudLightActive
+            case .cloudLightHover: return.cloudLightHover
+            case .cloudNormal: return.cloudNormal
+            case .cloudNormalActive: return.cloudNormalActive
+            case .cloudNormalHover: return.cloudNormalHover
+            case .greenDark: return.greenDark
+            case .greenDarkActive: return.greenDarkActive
+            case .greenDarkHover: return.greenDarkHover
+            case .greenDarker: return.greenDarker
+            case .greenLight: return.greenLight
+            case .greenLightActive: return.greenLightActive
+            case .greenLightHover: return.greenLightHover
+            case .greenNormal: return.greenNormal
+            case .greenNormalActive: return.greenNormalActive
+            case .greenNormalHover: return.greenNormalHover
+            case .inkDark: return.inkDark
+            case .inkDarkActive: return.inkDarkActive
+            case .inkDarkHover: return.inkDarkHover
+            case .inkLight: return.inkLight
+            case .inkLightActive: return.inkLightActive
+            case .inkLightHover: return.inkLightHover
+            case .inkNormal: return.inkNormal
+            case .inkNormalActive: return.inkNormalActive
+            case .inkNormalHover: return.inkNormalHover
+            case .orangeDark: return.orangeDark
+            case .orangeDarkActive: return.orangeDarkActive
+            case .orangeDarkHover: return.orangeDarkHover
+            case .orangeDarker: return.orangeDarker
+            case .orangeLight: return.orangeLight
+            case .orangeLightActive: return.orangeLightActive
+            case .orangeLightHover: return.orangeLightHover
+            case .orangeNormal: return.orangeNormal
+            case .orangeNormalActive: return.orangeNormalActive
+            case .orangeNormalHover: return.orangeNormalHover
+            case .productDark: return.productDark
+            case .productDarkActive: return.productDarkActive
+            case .productDarkHover: return.productDarkHover
+            case .productDarker: return.productDarker
+            case .productLight: return.productLight
+            case .productLightActive: return.productLightActive
+            case .productLightHover: return.productLightHover
+            case .productNormal: return.productNormal
+            case .productNormalActive: return.productNormalActive
+            case .productNormalHover: return.productNormalHover
+            case .redDark: return.redDark
+            case .redDarkActive: return.redDarkActive
+            case .redDarkHover: return.redDarkHover
+            case .redDarker: return.redDarker
+            case .redLight: return.redLight
+            case .redLightActive: return.redLightActive
+            case .redLightHover: return.redLightHover
+            case .redNormal: return.redNormal
+            case .redNormalActive: return.redNormalActive
+            case .redNormalHover: return.redNormalHover
+            case .socialFacebook: return.socialFacebook
+            case .socialFacebookActive: return.socialFacebookActive
+            case .socialFacebookHover: return.socialFacebookHover
+            case .whiteNormal: return.whiteNormal
+            case .whiteActive: return.whiteActive
+            case .whiteDarker: return.whiteDarker
+            case .whiteHover: return.whiteHover
+            case .whiteLighter: return.whiteLighter
+            default: return .label
+        }
+    }
 }
