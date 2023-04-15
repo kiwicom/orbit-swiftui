3808313bae93315198b6d4e4ba97ad8dc5282e20
Pavel Holec <pavel.holec@kiwi.com>
2023-04-15 21:05:41 +0200

    Add environment to `identifier` to propagate in both directions

--- Sources/Orbit/Support/Environment Keys/Identifier.swift
+++ Sources/Orbit/Support/Environment Keys/Identifier.swift
@@ -1,12 +1,27 @@
 import SwiftUI
 
+struct IdentifierKey: EnvironmentKey {
+    static var defaultValue: AnyHashable? = nil
+}
+
+public extension EnvironmentValues {
+
+    /// An Orbit identifier stored in a view’s environment.
+    var identifier: AnyHashable? {
+        get { self[IdentifierKey.self] }
+        set { self[IdentifierKey.self] = newValue }
+    }
+}
+
 public extension View {
 
     /// Binds a view’s identity to the given proxy value.
     ///
     /// This Orbit override adds the custom `IDPreferenceKey` preference on top of native identity.
     func identifier<ID: Hashable>(_ id: ID) -> some View {
-        self.id(id)
+        self
+            .environment(\.identifier, id)
+            .id(id)
             .anchorPreference(key: IDPreferenceKey.self, value: .bounds) { bounds in
                 [IDPreference(id: id, bounds: bounds)]
             }
