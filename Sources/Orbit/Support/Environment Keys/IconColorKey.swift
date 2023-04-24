3ec19455b11e93f9bee537099ee8044928a724ba
Pavel Holec <pavel.holec@kiwi.com>
2023-04-24 12:38:25 +0200

    Add `iconColor` and `textColor` environment keys

---
+++ Sources/Orbit/Support/Environment Keys/IconColorKey.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct IconColorKey: EnvironmentKey {
+    static var defaultValue: Color? = nil
+}
+
+public extension EnvironmentValues {
+
+    /// An icon color stored in a view’s environment, used for Orbit Icon component.
+    ///
+    /// This environment value serves as a replacement for non-public `foregroundColor` environment value.
+    /// This value has a priority over a `textColor`.
+    var iconColor: Color? {
+        get { self[IconColorKey.self] }
+        set { self[IconColorKey.self] = newValue }
+    }
+}
+
+public extension View {
+
+    /// Set the color for any Orbit `Icon` component within the view hierarchy.
+    ///
+    /// The `iconColor` value has a priority over the `textColor` environment value.
+    ///
+    /// - Parameters:
+    ///   - color: A color that will be used in Orbit `Icon` components.
+    ///    Pass `nil` to ignore environment icon color and to allow the system or the container to provide its own color. If a container-specific override doesn’t exist, the `textColor` color will be used.
+    func iconColor(_ color: Color?) -> some View {
+        environment(\.iconColor, color)
+    }
+}
---
+++ Sources/Orbit/Support/Environment Keys/TextColorKey.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+struct TextColorKey: EnvironmentKey {
+    static var defaultValue: Color? = nil
+}
+
+public extension EnvironmentValues {
+
+    /// A text color stored in a view’s environment, used for Orbit text based components, such as `Text`, `Heading` or `Icon`.
+    ///
+    /// This environment value serves as a replacement for non-public `foregroundColor` environment value.
+    var textColor: Color? {
+        get { self[TextColorKey.self] }
+        set { self[TextColorKey.self] = newValue }
+    }
+}
+
+public extension View {
+
+    /// Set the text color for any text based Orbit component within the view hierarchy.
+    ///
+    /// - Parameters:
+    ///   - color: A color that will be used in text based Orbit components such as `Text`, `Heading` or `Icon`.
+    ///    Pass `nil` to ignore environment text color and to allow the system or the container to provide its own color. If a container-specific override doesn’t exist, the `inkDark` color will be used.
+    func textColor(_ color: Color?) -> some View {
+        environment(\.textColor, color)
+    }
+}
