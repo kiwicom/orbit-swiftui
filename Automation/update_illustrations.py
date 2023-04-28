1850495c01b73f0139d00baa3a69bd814bb6976e
Pavel Holec <pavel.holec@kiwi.com>
2023-04-28 16:02:06 +0200

    Add resize and quantization support for Illustration updater

--- Automation/update_illustrations.py
+++ Automation/update_illustrations.py
@@ -3,15 +3,17 @@
 
 """
 This script:
-  1) Gets all currently defined illustration names from 'Illustrations.swift'
-  2) Downloads and rewrites all illustrations
+  1) Downloads original hi-res illustration resources
+  2) Resizes them into 1x, 2x and 3x sizes
 """
 
 import sys
 import os
-import re
 import pathlib
+import re
+import requests
 import shutil
+import subprocess
 import time
 from urllib.request import urlretrieve
 
@@ -40,31 +42,51 @@ content_template = '''{{
 }}
 '''
 
-sizes = [200, 400, 600]
-illustration_url_template = 'https://images.kiwi.com/illustrations/0x{size}/{illustrationName}.png'
+illustration_heights = [200, 400, 600]
+illustration_url_template = 'https://images.kiwi.com/illustrations/originals/{illustrationName}.png'
 source_filename = 'Illustrations.swift'
 
-def uppercase_first_letter(s):
-  return s[:1].upper() + s[1:] if str else ''
-
 def get_illustration_names(codePath):
-  p = re.compile(r'case\s+(\w+)')
+  url = 'https://raw.githubusercontent.com/kiwicom/orbit/master/packages/orbit-components/src/Illustration/consts.mts'
+  response = requests.get(url, stream=True)
+
   names = []
-  with open(codePath, 'r') as illustrationCode:
-    for line in illustrationCode:
-      for case in p.findall(line):
-        if case == 'none':
-          continue
-        names.append(uppercase_first_letter(case))
+
+  for line in response.iter_lines():
+    match = re.search('\"(\w+)\"', line.decode('utf-8'))
+
+    if match:
+      names.append(match.group(1))
+
   return names
 
-def download_illustration(illustrationName, sizeIndex, assetDownloadedPath):
-  url = illustration_url_template.format(illustrationName = illustrationName, size = sizes[sizeIndex-1])
-  filename = f'{illustrationName}@{sizeIndex}x.png'
-  filepath = assetDownloadedPath.joinpath(filename)
-  print(f'Downloading [{url}] -> [{filename}] ...')
+def download_and_resize_illustration(illustrationName, assetDownloadedPath):
+  url = illustration_url_template.format(illustrationName = illustrationName)
+  originalFilename = f'{illustrationName}@full.png'
+  originalFilepath = assetDownloadedPath.joinpath(originalFilename)
+  print(f'Downloading [{url}] ...')
   try:
-    urlretrieve(url, filepath)
+    urlretrieve(url, originalFilepath)
+
+    print(f'Resizing [{illustrationName}] -> ', end='')
+
+    for idx, height in enumerate(illustration_heights):
+      print(f'[{height}@{(idx+1)}x] ... ', end='')
+
+      resizedFilename = f'{illustrationName}@{(idx+1)}x.png'
+      resizedFilePath = assetDownloadedPath.joinpath(resizedFilename)
+
+      # Resize
+      command = f'convert {originalFilepath} -resize x{height} -filter Lanczos -quality 100 {resizedFilePath}'
+      subprocess.run(command, shell=True)
+
+      # Optimize
+      command = f'pngquant --speed 1 --quality=65-85 --strip -f -o {resizedFilePath} -- {resizedFilePath}'
+      subprocess.run(command, shell=True)
+
+    print()
+    os.remove(originalFilepath)
+    
   except Exception as e:
     print(f'❗️{e}')
 
@@ -94,8 +116,4 @@ if __name__ == "__main__":
     with open(folderPath.joinpath('Contents.json'), "w") as contentFile:
       contentFile.write(content_template.format(name = illustration))
 
-    time.sleep(1)
-
-    for sizeIndex in range(1, 4):
-      download_illustration(illustration, sizeIndex, folderPath)
-      time.sleep(0.5)
+    download_and_resize_illustration(illustration, folderPath)
