51ce063a2ec6004888468eed162952e6566889c2
Nikolay Buldkov <nikolay.buldakov@kiwi.com>
2023-08-14 21:50:04 +0200

    Make properties of TextRepresentableEnvironment public

--- Sources/Orbit/Support/Text/TextRepresentable.swift
+++ Sources/Orbit/Support/Text/TextRepresentable.swift
@@ -10,11 +10,30 @@ public protocol TextRepresentable {
 
 /// Environment values required for proper formatting of concatenated Orbit text components.
 public struct TextRepresentableEnvironment {
-    let iconColor: Color?
-    let iconSize: CGFloat?
-    let textAccentColor: Color?
-    let textColor: Color?
-    let textFontWeight: Font.Weight?
-    let textSize: CGFloat?
-    let sizeCategory: ContentSizeCategory
+
+    public let iconColor: Color?
+    public let iconSize: CGFloat?
+    public let textAccentColor: Color?
+    public let textColor: Color?
+    public let textFontWeight: Font.Weight?
+    public let textSize: CGFloat?
+    public let sizeCategory: ContentSizeCategory
+
+    public init(
+        iconColor: Color?,
+        iconSize: CGFloat?,
+        textAccentColor: Color?,
+        textColor: Color?,
+        textFontWeight: Font.Weight?,
+        textSize: CGFloat?,
+        sizeCategory: ContentSizeCategory
+    ) {
+        self.iconColor = iconColor
+        self.iconSize = iconSize
+        self.textAccentColor = textAccentColor
+        self.textColor = textColor
+        self.textFontWeight = textFontWeight
+        self.textSize = textSize
+        self.sizeCategory = sizeCategory
+    }
 }
