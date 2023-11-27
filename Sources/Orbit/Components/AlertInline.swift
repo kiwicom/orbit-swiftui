8ab3741d3196364987d8c0b415a47310975d282e
Pavel Holec <pavel.holec@kiwi.com>
2023-11-27 15:42:18 +0100

    Extract `AlertInlineButtonsBuilder` to a separate file

--- Sources/Orbit/Components/AlertInline.swift
+++ Sources/Orbit/Components/AlertInline.swift
@@ -54,35 +54,6 @@ public extension AlertInline {
     }
 }
 
-/// A builder that constructs buttons for the ``AlertInline`` component.
-@resultBuilder
-public enum AlertInlineButtonsBuilder {
-
-    public static func buildBlock(_ emptyView: EmptyView) -> EmptyView {
-        emptyView
-    }
-
-    public static func buildBlock(_ button: some View) -> some View {
-        button
-            .suppressButtonStyle()
-            .buttonStyle(AlertInlineButtonStyle())
-            .padding(.trailing, .small)
-            .accessibility(.alertButtonPrimary)
-    }
-
-    public static func buildOptional<V: View>(_ component: V?) -> V? {
-        component
-    }
-
-    public static func buildEither<T: View, F: View>(first view: T) -> _ConditionalContent<T, F> {
-        .init(content: .trueView(view))
-    }
-
-    public static func buildEither<T: View, F: View>(second view: F) -> _ConditionalContent<T, F> {
-        .init(content: .falseView(view))
-    }
-}
-
 // MARK: - Previews
 struct AlertInlinePreviews: PreviewProvider {
 
